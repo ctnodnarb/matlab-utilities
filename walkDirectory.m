81b91d71f96fb77a2bf436b863f543083ac159e9
Brandon Carroll <ctnodnarb@gmail.com>
2015-06-22 17:48:12 -0400

    Change walkDirectory to return a column vector.
    Also cleaned up some of its comments.

--- walkDirectory.m
+++ walkDirectory.m
@@ -9,28 +9,27 @@ function entries = walkDirectory(directory, pattern)
 % directory and its subdirectories:
 %	walk('.', '*.txt')
 
+% Get a list containing the folder and all its subfolders.
 if regexp(computer, '^GLNX.*')
 	% Linux separates paths with colons
 	directories_to_search = strsplit(genpath(directory), ':');
 else
 	% Windows separates paths with semicolons
-	% If you're running an older version of Matlab that doesn't have the
-	% strsplit function, use the alternate (commented out) code below
-	% instead
 	directories_to_search = strsplit(genpath(directory), ';');
 end
 
 % directories_to_search should now be a cell array containing a list of
 % the specified directory and ALL of its subdirectories, with the final
-% element being an empty string.
-
+% element being an empty string.  Loop through all those directories
+% (excluding the final exmpty string at the end) and gather all the file
+% names that match the pattern.
 entries = [];
 for i = 1:length(directories_to_search)-1
 	folder = directories_to_search{i};
 	dir_entries = dir(fullfile(folder, pattern));
 	if ~isempty(dir_entries)
-		entries = [entries, arrayfun(@fullfile, ...
-				repmat({folder}, 1, length(dir_entries)), ...
-				{dir_entries.name})];
+		entries = [entries; arrayfun(@fullfile, ...
+				repmat({folder}, length(dir_entries), 1), ...
+				{dir_entries.name}')];
 	end
 end
